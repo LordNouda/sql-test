5efbc0e10bbe4a40a65d72e01efeea082c6a8195
Manuel Succo <msucco@mac.com>
2018-05-09 11:56:09 +0200

    create table
    created databases "cars" and "new" with tables in SQL.

---
+++ 01_sql.py
@@ -0,0 +1,31 @@
+# create a SQLite3 database and table
+
+# import the sqlite3 library
+import sqlite3
+
+# create a new database if the database doesn't already exist
+conn = sqlite3.connect("new.db")
+
+# get a cursor object used to execute SQL commands
+cursor = conn.cursor()
+
+# create a table
+cursor.execute("DROP TABLE population;")
+cursor.execute("""
+CREATE TABLE population
+(city TEXT, state TEXT, population INT)
+""")
+
+# close the database connection
+conn.close()
+
+# In[]: Homework
+
+conn = sqlite3.connect("cars.db")
+c = conn.cursor()
+c.execute("DROP TABLE inventory;")
+c.execute("""
+CREATE TABLE inventory
+(make TEXT, model TEXT, quantity INT)
+""")
+conn.close()
