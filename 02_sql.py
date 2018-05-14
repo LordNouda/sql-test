f84763e3c8624a38afaa7f068677813394220530
Manuel Succo <msucco@mac.com>
2018-05-14 11:31:03 +0200

    SQL: INSERT
    Changed to try/except statement to catch database operational errors.

--- 02_sql.py
+++ 02_sql.py
@@ -9,18 +9,22 @@ connection = sqlite3.connect("new.db")
 # get a cursor object used to execute SQL commands
 cursor = connection.cursor()
 
-# insert data
-cursor.execute("""
-    INSERT INTO population
-    VALUES('New York City', 'NY', 8400000)
-""")
-cursor.execute("""
-    INSERT INTO population
-    VALUES('San Francisco', 'CA', 800000)
-""")
+try:
+    # insert data
+    cursor.execute("""
+        INSERT INTO populations
+        VALUES('New York City', 'NY', 8400000)
+    """)
+    cursor.execute("""
+        INSERT INTO populations
+        VALUES('San Francisco', 'CA', 800000)
+    """)
 
-# commit the changes
-connection.commit()
+    # commit the changes
+    connection.commit()
+except sqlite3.OperationalError as err:
+    print("Oops!  Something went wrong. Try again...")
+    print('Operational error:', err)
 
 # close the database connection
 connection.close()
